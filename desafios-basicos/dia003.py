97d2dfe110b3842d77adabd68941c9f31518f7f1
Gabriel Rocha Pimentel <gabriel-rocha-pimentel@users.noreply.github.com>
2024-11-08 14:28:47 -0300

    next day, new file chalenge

---
+++ desafios-basicos/dia003.py
@@ -0,0 +1,10 @@
+# Conversão de Temperatura
+# Converta de celcius para fahrenheit
+# Formula de Fahrenheit F=(C×1.8)+32
+celcius = 25
+
+print(f"A temperatura atual em celcius é {celcius}")
+
+fahrenheit = (celcius * 1.8) + 32
+
+print(f"A conversão para fahrenheit é {fahrenheit}")
