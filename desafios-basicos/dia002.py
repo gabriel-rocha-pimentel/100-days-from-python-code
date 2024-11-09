4501fec366b8751658fbcc22ce015848143f9a7e
Gabriel Rocha Pimentel <gabriel-rocha-pimentel@users.noreply.github.com>
2024-11-09 11:15:42 -0300

    atualizado desafio 2

--- desafios-basicos/dia002.py
+++ desafios-basicos/dia002.py
@@ -1,9 +1,19 @@
 # Cálculo de Soma
 # Receba dois números e imprima a soma deles.
-print("Farei a soma de dois números e mostrarei o resultado.")
-a = int(input("Por favor insira o primeiro número : "))
-b = int(input("Por favor insira o segundo número : "))
+start = "SIM"
 
-soma = a + b
+while start[0] == "S":
+  print("Farei a soma de dois números e mostrarei o resultado.")
 
-print(f"A soma de {a} mais {b} é : {soma}")
+  a = int(input("Por favor insira o primeiro número : "))
+  b = int(input("Por favor insira o segundo número : "))
+
+  soma = a + b
+
+  print(f"A soma de {a} mais {b} é : {soma}")
+
+  start = str(input("\nQuer continuar ? Sim ou Não\n>>>\t")).upper()
+
+  if start[0] != "N":
+    print("Por favor, insira [NÃO] para parar o programa !")
+    
